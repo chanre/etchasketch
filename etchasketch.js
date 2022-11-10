9eac091cacfc670dac31e3e50b2ed2f323dc9a6a
Ryan <ryan.e.chan@gmail.com>
2022-11-09 21:59:17 -0500

    Added click and drag drawing + options menu
    Basic options menu with a color picker, rainbow option, an eraser
    a clear all and size slider has been created. No functionality is
    implemented yet.

--- etchasketch.js
+++ etchasketch.js
@@ -5,10 +5,37 @@ function createSquares(squareSize) {
     grid.style.gridTemplateColumns = `repeat(${squareSize}, 1fr)`;
     for (let i = 0; i < total; i++)  {
         const square = document.createElement("div");
-        square.style.border = "1px solid blue";
+        square.style.backgroundColor = "white";
+        square.setAttribute("draggable", false);
+        square.addEventListener("mousedown", colourGrid);
+        square.addEventListener("mouseenter", colourGrid);
         grid.appendChild(square).className = "square";
     }
     
 }
 
-createSquares(64);
+function colourGrid(e) {
+    e.preventDefault();
+    const square = e.target;
+    if (e.buttons === 0) return;
+    square.style.backgroundColor = "black";
+}
+
+function clearGrid() {
+    const grid = document.querySelector(".square");
+}
+
+let gridSize = 40;
+const sizeText = document.querySelector(".size");
+const slider = document.querySelector(".slider");
+
+slider.onmousemove = (e) => {
+    sizeText.textContent = `${parseInt(e.target.value) + 15} x ${parseInt(e.target.value) + 15}`;
+}
+
+slider.onchange = (e) => {
+    gridSize = e.target.value;
+    createSquares(gridSize);
+}
+
+createSquares(40);
